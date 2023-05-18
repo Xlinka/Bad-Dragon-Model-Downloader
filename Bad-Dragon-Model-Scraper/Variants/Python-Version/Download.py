d2755d0e3f4f8636d85af4fbca31073ac0866786
xLinka <Oliver.dunnington@outlook.com>
2023-05-18 18:47:47 +0100

    Squish Python
    Squish Python

--- Bad-Dragon-Model-Scraper/Variants/Python-Version/Download.py
+++ Bad-Dragon-Model-Scraper/Variants/Python-Version/Download.py
@@ -1,6 +1,4 @@
-import os
-import requests
-import json
+import os, requests, json
 
 class DownloadableProduct:
     def __init__(self, name, preview_object_url, preview_texture_map_url, preview_normal_map_url):
@@ -9,57 +7,25 @@ class DownloadableProduct:
         self.preview_texture_map_url = preview_texture_map_url
         self.preview_normal_map_url = preview_normal_map_url
 
-PRODUCT_URL = "https://bad-dragon.com/api/products"
-SAVE_DIR = "models"
-
-def main():
-    os.makedirs(SAVE_DIR, exist_ok=True)
-
-    print(f"Saving objects to {SAVE_DIR}")
-
-    products = load_products()
-    for product in process_products(products):
-        download_product(product, SAVE_DIR)
-
-def load_products():
-    response = requests.get(PRODUCT_URL, headers={'User-Agent': 'Mozilla/5.0 (compatible; AcmeInc/1.0)'})
-    return response.json()
-
-def process_products(products):
-    processed_products = []
-    for product in products:
-        name = product.get('sku')
-
-        preview_object_url = product.get('previewObjModel', {}).get('url')
-        preview_texture_map_url = product.get('previewTextureMap', {}).get('url')
-        preview_normal_map_url = product.get('previewNormalMap', {}).get('url')
-
-        if not name or not preview_object_url or not preview_texture_map_url or not preview_normal_map_url:
-            continue
-
-        processed_products.append(DownloadableProduct(name, preview_object_url, preview_texture_map_url, preview_normal_map_url))
-    
-    return processed_products
-
 def download_file(url, path):
-    if os.path.exists(path):
-        print(f"Already downloaded {path}")
-        return
-
-    print(f"Downloading {path}")
-
-    response = requests.get(url)
-
-    with open(path, 'wb') as f:
-        f.write(response.content)
-
-def download_product(product, save_dir):
-    product_dir = os.path.join(save_dir, product.name)
-    os.makedirs(product_dir, exist_ok=True)
-
-    download_file(product.preview_object_url, os.path.join(product_dir, f"{product.name}.obj"))
-    download_file(product.preview_texture_map_url, os.path.join(product_dir, f"{product.name}_texture_map.png"))
-    download_file(product.preview_normal_map_url, os.path.join(product_dir, f"{product.name}_normal_map.png"))
+    if not os.path.exists(path):
+        print(f"Downloading {path}")
+        with open(path, 'wb') as f:
+            f.write(requests.get(url).content)
 
 if __name__ == "__main__":
-    main()
+    PRODUCT_URL = "https://bad-dragon.com/api/products"
+    SAVE_DIR = "models"
+    os.makedirs(SAVE_DIR, exist_ok=True)
+    print(f"Saving objects to {SAVE_DIR}")
+
+    products = requests.get(PRODUCT_URL, headers={'User-Agent': 'Mozilla/5.0 (compatible; AcmeInc/1.0)'}).json()
+    for product in products:
+        name, obj_url, tex_url, norm_url = product.get('sku'), product.get('previewObjModel', {}).get('url'), product.get('previewTextureMap', {}).get('url'), product.get('previewNormalMap', {}).get('url')
+        if name and obj_url and tex_url and norm_url:
+            dp = DownloadableProduct(name, obj_url, tex_url, norm_url)
+            product_dir = os.path.join(SAVE_DIR, dp.name)
+            os.makedirs(product_dir, exist_ok=True)
+            download_file(dp.preview_object_url, os.path.join(product_dir, f"{dp.name}.obj"))
+            download_file(dp.preview_texture_map_url, os.path.join(product_dir, f"{dp.name}_texture_map.png"))
+            download_file(dp.preview_normal_map_url, os.path.join(product_dir, f"{dp.name}_normal_map.png"))
