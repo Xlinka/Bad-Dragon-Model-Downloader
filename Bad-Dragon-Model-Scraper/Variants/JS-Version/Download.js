16ef48ed58cb371cb0347b7fa7be1bfde5ac508d
Sharkmare <34294231+Sharkmare@users.noreply.github.com>
2023-05-18 19:39:13 +0200

    golfin

--- Bad-Dragon-Model-Scraper/Variants/JS-Version/Download.js
+++ Bad-Dragon-Model-Scraper/Variants/JS-Version/Download.js
@@ -3,61 +3,48 @@ const fs = require('fs');
 const path = require('path');
 
 class DownloadableProduct {
-    constructor(name, previewObjectUrl, previewTextureMapUrl, previewNormalMapUrl) {
-        this.name = name;
-        this.previewObjectUrl = previewObjectUrl;
-        this.previewTextureMapUrl = previewTextureMapUrl;
-        this.previewNormalMapUrl = previewNormalMapUrl;
-    }
+  constructor(name, previewObjectUrl, previewTextureMapUrl, previewNormalMapUrl) {
+    Object.assign(this, { name, previewObjectUrl, previewTextureMapUrl, previewNormalMapUrl });
+  }
 }
 
 const PRODUCT_URL = "https://bad-dragon.com/api/products";
 const SAVE_DIR = "models";
 
-async function main() {
-    if (!fs.existsSync(SAVE_DIR)) fs.mkdirSync(SAVE_DIR);
-    console.log(`Saving objects to ${SAVE_DIR}`);
+const main = async () => {
+  fs.existsSync(SAVE_DIR) || fs.mkdirSync(SAVE_DIR);
+  console.log(`Saving objects to ${SAVE_DIR}`);
 
-    const products = await loadProducts();
-    for (const product of processProducts(products))
-        await downloadProduct(product, SAVE_DIR);
-}
+  const products = await loadProducts();
+  for (const product of processProducts(products)) {
+    await downloadProduct(product, SAVE_DIR);
+  }
+};
 
-async function loadProducts() {
-    const response = await axios.get(PRODUCT_URL, { headers: {'User-Agent': 'Mozilla/5.0 (compatible; AcmeInc/1.0)'}});
-    return response.data;
-}
+const loadProducts = async () => (await axios.get(PRODUCT_URL, { headers: { 'User-Agent': 'Mozilla/5.0 (compatible; AcmeInc/1.0)' } })).data;
 
-function processProducts(products) {
-    return products.map(product => {
-        const { sku: name, previewObjModel, previewTextureMap, previewNormalMap } = product;
+const processProducts = (products) =>
+  products.map(({ sku: name, previewObjModel, previewTextureMap, previewNormalMap }) => {
+    if (!name || !previewObjModel || !previewTextureMap || !previewNormalMap) return null;
+    return new DownloadableProduct(name, previewObjModel.url, previewTextureMap.url, previewNormalMap.url);
+  }).filter(Boolean);
 
-        if (!name || !previewObjModel || !previewTextureMap || !previewNormalMap)
-            return null;
+const downloadFile = async (url, filePath) => {
+  if (fs.existsSync(filePath)) return console.log(`Already downloaded ${filePath}`);
+  console.log(`Downloading ${filePath}`);
+  fs.writeFileSync(filePath, Buffer.from((await axios.get(url, { responseType: 'arraybuffer' })).data, 'binary'));
+};
 
-        return new DownloadableProduct(name, previewObjModel.url, previewTextureMap.url, previewNormalMap.url);
-    }).filter(Boolean);
-}
+const downloadProduct = async (product, saveDir) => {
+  const productDir = path.join(saveDir, product.name);
 
-async function downloadFile(url, filePath) {
-    if (fs.existsSync(filePath)) {
-        console.log(`Already downloaded ${filePath}`);
-        return;
-    }
+  fs.existsSync(productDir) || fs.mkdirSync(productDir);
 
-    console.log(`Downloading ${filePath}`);
-    const response = await axios.get(url, { responseType: 'arraybuffer' });
-    fs.writeFileSync(filePath, Buffer.from(response.data, 'binary'));
-}
-
-async function downloadProduct(product, saveDir) {
-    const productDir = path.join(saveDir, product.name);
-
-    if (!fs.existsSync(productDir)) fs.mkdirSync(productDir);
-
-    await downloadFile(product.previewObjectUrl, path.join(productDir, `${product.name}.obj`));
-    await downloadFile(product.previewTextureMapUrl, path.join(productDir, `${product.name}_texture_map.png`));
-    await downloadFile(product.previewNormalMapUrl, path.join(productDir, `${product.name}_normal_map.png`));
-}
+  await Promise.all([
+    downloadFile(product.previewObjectUrl, path.join(productDir, `${product.name}.obj`)),
+    downloadFile(product.previewTextureMapUrl, path.join(productDir, `${product.name}_texture_map.png`)),
+    downloadFile(product.previewNormalMapUrl, path.join(productDir, `${product.name}_normal_map.png`))
+  ]);
+};
 
 main().catch(console.error);
