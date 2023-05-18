43f23153b5e174a59aea7e908040524c149d73ff
Sharkmare <34294231+Sharkmare@users.noreply.github.com>
2023-05-18 19:26:34 +0200

    codegolf me daddy

--- Bad-Dragon-Model-Scraper/Variants/JS-Version/Download.js
+++ Bad-Dragon-Model-Scraper/Variants/JS-Version/Download.js
@@ -15,61 +15,49 @@ const PRODUCT_URL = "https://bad-dragon.com/api/products";
 const SAVE_DIR = "models";
 
 async function main() {
-    if (!fs.existsSync(SAVE_DIR)) {
-        fs.mkdirSync(SAVE_DIR);
-    }
-
+    if (!fs.existsSync(SAVE_DIR)) fs.mkdirSync(SAVE_DIR);
     console.log(`Saving objects to ${SAVE_DIR}`);
 
     const products = await loadProducts();
-    for (const product of processProducts(products)) {
+    for (const product of processProducts(products))
         await downloadProduct(product, SAVE_DIR);
-    }
 }
 
 async function loadProducts() {
-    const response = await axios.get(PRODUCT_URL, {headers: {'User-Agent': 'Mozilla/5.0 (compatible; AcmeInc/1.0)'}});
+    const response = await axios.get(PRODUCT_URL, { headers: {'User-Agent': 'Mozilla/5.0 (compatible; AcmeInc/1.0)'}});
     return response.data;
 }
 
 function processProducts(products) {
     return products.map(product => {
-        const name = product.sku || null;
+        const { sku: name, previewObjModel, previewTextureMap, previewNormalMap } = product;
 
-        const previewObjectUrl = product.previewObjModel ? product.previewObjModel.url : null;
-        const previewTextureMapUrl = product.previewTextureMap ? product.previewTextureMap.url : null;
-        const previewNormalMapUrl = product.previewNormalMap ? product.previewNormalMap.url : null;
-
-        if (!name || !previewObjectUrl || !previewTextureMapUrl || !previewNormalMapUrl) {
+        if (!name || !previewObjModel || !previewTextureMap || !previewNormalMap)
             return null;
-        }
 
-        return new DownloadableProduct(name, previewObjectUrl, previewTextureMapUrl, previewNormalMapUrl);
-    }).filter(product => product !== null);
+        return new DownloadableProduct(name, previewObjModel.url, previewTextureMap.url, previewNormalMap.url);
+    }).filter(Boolean);
 }
 
-async function downloadFile(url, path) {
-    if (fs.existsSync(path)) {
-        console.log(`Already downloaded ${path}`);
+async function downloadFile(url, filePath) {
+    if (fs.existsSync(filePath)) {
+        console.log(`Already downloaded ${filePath}`);
         return;
     }
 
-    console.log(`Downloading ${path}`);
-
-    const response = await axios.get(url, {responseType: 'arraybuffer'});
-    fs.writeFileSync(path, new Buffer.from(response.data), 'binary');
+    console.log(`Downloading ${filePath}`);
+    const response = await axios.get(url, { responseType: 'arraybuffer' });
+    fs.writeFileSync(filePath, Buffer.from(response.data, 'binary'));
 }
 
 async function downloadProduct(product, saveDir) {
     const productDir = path.join(saveDir, product.name);
 
-    if (!fs.existsSync(productDir)) {
-        fs.mkdirSync(productDir);
-    }
+    if (!fs.existsSync(productDir)) fs.mkdirSync(productDir);
 
     await downloadFile(product.previewObjectUrl, path.join(productDir, `${product.name}.obj`));
     await downloadFile(product.previewTextureMapUrl, path.join(productDir, `${product.name}_texture_map.png`));
     await downloadFile(product.previewNormalMapUrl, path.join(productDir, `${product.name}_normal_map.png`));
 }
 
-main().catch(err => console.error(err));
+main().catch(console.error);
